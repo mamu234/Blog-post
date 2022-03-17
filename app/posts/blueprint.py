f237cec801ae59852326db8fb8adee3a2a65dd51
Caroline .A. Maunda <78535921+mamu234@users.noreply.github.com>
2022-03-17 20:54:13 +0300

    Delete blueprint.py

--- app/posts/blueprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-posts = Blueprint('posts', __name__, template_folder='templates')
-
-
-@posts.route('/')
-def posts_list():
- return render_template('posts/posts.html')
